5f2eee06686d1490d0f0672d7f67e77d5324178a
Kahvi Patel <kahvi.patel@shopify.com>
2021-10-25 23:10:01 -0700

    No more relative paths

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-export DF_HOME=~/dotfiles
+cd ~
 
 # Linking everything
-ln -sf $DF_HOME/.zshrc ~/.zshrc 
-ln -sf $DF_HOME/.vimrc ~/.vimrc 
-ln -sf $DF_HOME/.tmux.conf ~/.tmux.conf
-ln -sf $DF_HOME/.gitconfig ~/.gitconfig
+ln -sf dotfiles/.zshrc .zshrc 
+ln -sf dotfiles/.vimrc .vimrc 
+ln -sf dotfiles/.tmux.conf .tmux.conf
+ln -sf dotfiles/.gitconfig .gitconfig
 
 # installing
 sudo apt-get install -y fzf bat neovim thefuck
 
 # Moving vimrc for nvim
 mkdir -p .config/nvim
-ln -sf  $DF_HOME/init.vim .config/nvim/init.vim
+ln -sf  dotfiles/init.vim .config/nvim/init.vim
 
 # Install oh-my-zsh stuff
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Install nvim plugin manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \ 
