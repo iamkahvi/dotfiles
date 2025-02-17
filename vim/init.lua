73916afa8ecc4795f5ce9a81ef7fe2f76168b68f
Kahvi Patel <iamkahvi@gmail.com>
2025-02-17 03:53:00 -0400

    nvim init.lua added

---
+++ vim/init.lua
@@ -0,0 +1,36 @@
+vim.cmd("source ~/.vimrc")
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  pattern = ":",
+  callback = function()
+    local cmd = vim.fn.getcmdline()
+    if cmd:match("^%d+$") then
+      vim.cmd("normal! zz")
+    end
+  end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "junegunn/goyo.vim" },
+  { "scrooloose/nerdcommenter" },
+  { "preservim/nerdtree" },  -- Updated repo for NERDTree
+  { "tpope/vim-surround" },
+  { "rust-lang/rust.vim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+})
+
+vim.cmd.colorscheme "catppuccin-frappe"
+
